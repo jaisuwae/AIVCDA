cf7d23a7084c7016c56f402bcdb34ea2f5905f3e
Jaisuwae <jaisuwae@gmail.com>
2026-04-23 20:29:18 +0530

    Delete AIVCDA directory

--- AIVCDA/test_volume_attrs.py
+++
@@ -1,7 +0,0 @@
-from pycaw.pycaw import AudioUtilities
-def test_dir():
-    devices = AudioUtilities.GetSpeakers()
-    print(f"Attributes: {dir(devices)}")
-
-if __name__ == "__main__":
-    test_dir()
